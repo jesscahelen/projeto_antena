41eef9c3a0bf7f1500a71c2169719419e95ae5b0
Jéssica Rosado <jessicahelen@live.com>
2019-09-16 22:07:48 -0300

    alterações na view

--- src/main/java/hello/MainServer.java
+++ src/main/java/hello/MainServer.java
@@ -20,7 +20,7 @@ public class MainServer {
         if (process.environment().get("PORT") != null) {
             port = Integer.parseInt(process.environment().get("PORT"));
         } else {
-            port = 8083;
+            port = 8080;
         }
         port(port);
 
